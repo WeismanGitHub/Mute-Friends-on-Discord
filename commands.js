5324b0bd35f8aaf8e87d7106e1bee5245c559ee0
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-03-07 07:09:42 -0800

    created

---
+++ commands.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+module.exports = {
+    'LEFT CTRL LEFT ALT NUMPAD 7': {
+        discordId: process.env.ID_1
+    },
+    'LEFT CTRL LEFT ALT NUMPAD 8': {
+        discordId: process.env.ID_2
+    },
+    'LEFT CTRL LEFT ALT NUMPAD 9': {
+        discordId: process.env.ID_3
+    },
+}
